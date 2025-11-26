4947049c67dcc4c9f14b108ddf5b3a46fc7610d7
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-11-26 15:21:44 +0100

    Refactor : Rename OrederitemDtorequest to OrderItemDtoRequest and add validation annotations

--- src/main/java/com/microtech/smartshop/dto/OrderItemDtoRequest.java
+++ src/main/java/com/microtech/smartshop/dto/OrderItemDtoRequest.java
@@ -1,12 +1,17 @@
 package com.microtech.smartshop.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data @Builder @AllArgsConstructor @NoArgsConstructor
-public class OrederitemDtorequest {
+public class OrderItemDtoRequest {
+    @NotNull(message = "L'ID du produit est obligatoire")
     private Long productId;
+
+    @Min(value = 1, message = "La quantité doit être au moins 1")
     private int quantity;
 }
