6faee30e70f1aaa236daf701b7673b787ab62648
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-11-27 12:09:50 +0100

    Refactor : Implement WebConfig for interceptor configuration

---
+++ src/main/java/com/microtech/smartshop/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.microtech.smartshop.config;
+
+import com.microtech.smartshop.interceptor.AuthInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/auth/login",
+                        "/auth/logout",
+                        "/swagger-ui.html",
+                        "/swagger-ui/**",
+                        "/v3/api-docs/**"
+                );
+    }
+}
