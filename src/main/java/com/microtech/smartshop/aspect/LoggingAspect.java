f955f2f43fc6ac5a267ba27bdfa19359f4fd23e5
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-11-27 12:11:24 +0100

    Refactor : Implement LoggingAspect for method execution time logging

---
+++ src/main/java/com/microtech/smartshop/aspect/LoggingAspect.java
@@ -0,0 +1,23 @@
+package com.microtech.smartshop.aspect;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+public class LoggingAspect {
+
+    @Around("execution(* com.microtech.smartshop.service..*(..)) || execution(* com.microtech.smartshop.Controller..*(..))")
+    public  Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
+        long executionTime = System.currentTimeMillis() - start;
+        log.info("{} executed in {} ms", joinPoint.getSignature(), executionTime);
+        return result;
+    }
+}
