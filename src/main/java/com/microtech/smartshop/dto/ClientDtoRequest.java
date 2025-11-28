2e73eca5bc5705b5f19bcee4b803055fbca0b750
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-11-28 09:41:34 +0100

    Refactor : Add validation annotations to ClientDtoRequest fields

--- src/main/java/com/microtech/smartshop/dto/ClientDtoRequest.java
+++ src/main/java/com/microtech/smartshop/dto/ClientDtoRequest.java
@@ -1,6 +1,9 @@
 package com.microtech.smartshop.dto;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,11 +15,17 @@ import lombok.NoArgsConstructor;
 @Builder
 public class ClientDtoRequest {
 
+    @NotBlank(message = "full name is required")
     private String fullName;
+
+    @Email(message = "email is required")
     private String email;
 
+    @NotBlank(message = "username is required")
     private String Username;
 
+
+    @NotNull(message = "password is required")
     private String Password;
 
 
