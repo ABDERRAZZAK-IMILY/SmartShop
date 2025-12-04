8a4f278bfbb3f0f24501207cf912e051ac144c7e
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-12-04 12:08:47 +0100

    Refactor : Add custom query methods to ClientRepository for enhanced data retrieval

--- src/main/java/com/microtech/smartshop/repository/ClientRepository.java
+++ src/main/java/com/microtech/smartshop/repository/ClientRepository.java
@@ -2,7 +2,11 @@ package com.microtech.smartshop.repository;
 
 import com.microtech.smartshop.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.List;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
     Optional<Client> findByEmail(String email);
